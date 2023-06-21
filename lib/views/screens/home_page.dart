9078c133e3ab7fd4ee1ab278f6e2676b09ab1c7e
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-06-21 20:57:09 +0530

    Added GridView Layout for displaying wallpapers

--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -28,62 +28,94 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 5.0,
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 8.0),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 234, 228, 228),
-              border: Border.all(
-                  // color: const Color.fromARGB(255, 19, 106, 22),
-                  ),
-              borderRadius: BorderRadius.circular(25),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 5.0,
             ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 18.0),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Search Wallpapers',
-                        hintStyle:
-                            TextStyle(color: Color.fromARGB(255, 76, 75, 75)),
-                        border: InputBorder.none,
-                      ),
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 19, 106, 22),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 8.0),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 234, 228, 228),
+                border: Border.all(
+                    // color: const Color.fromARGB(255, 19, 106, 22),
+                    ),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 18.0),
+                child: Row(
+                  children: [
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search Wallpapers',
+                          hintStyle:
+                              TextStyle(color: Color.fromARGB(255, 76, 75, 75)),
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 19, 106, 22),
+                        ),
                       ),
                     ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      print('Search');
-                    },
-                    child: const Icon(
-                      Icons.search,
-                      color: Colors.black,
-                    ),
-                  )
-                ],
+                    InkWell(
+                      onTap: () {
+                        print('Search');
+                      },
+                      child: const Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 15.0,
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 30,
-                itemBuilder: (context, index) => CatBlock()),
-          )
-        ],
+            const SizedBox(
+              height: 15.0,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 30,
+                  itemBuilder: (context, index) => CatBlock()),
+            ),
+
+            // GridView for displaying wallpapers
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              height: MediaQuery.of(context).size.height,
+              child: GridView.builder(
+                physics: BouncingScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 7,
+                    mainAxisExtent: 400),
+                itemCount: 20,
+                itemBuilder: ((context, index) => Container(
+                      height: 500,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.network(
+                            height: 500,
+                            width: 50,
+                            fit: BoxFit.cover,
+                            'https://images.pexels.com/photos/10394779/pexels-photo-10394779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                      ),
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
