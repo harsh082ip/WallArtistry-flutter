a4de1cd4100cec5674dff03b2416ad7e09e10e26
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-06-28 04:33:06 +0530

    Added Option to set wallpaers in Home Screen

--- lib/views/screens/full_screen.dart
+++ lib/views/screens/full_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
 import 'package:open_file_plus/open_file_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:async_wallpaper/async_wallpaper.dart';
 
 class FullScreen extends StatefulWidget {
   String imgUrl;
@@ -32,53 +33,85 @@ class _FullScreenState extends State<FullScreen> {
   void initState() {
     super.initState();
     requestStoragePermission(Permission.photos);
+    fPath = '';
+    count = 0;
   }
 
+  int count = 0;
   double? _progress;
+
+  void setWallpapers(String path) async {
+    await AsyncWallpaper.setWallpaperFromFile(
+      filePath: path,
+      wallpaperLocation: AsyncWallpaper.HOME_SCREEN,
+      goToHome: true,
+      toastDetails: ToastDetails.success(),
+      errorToastDetails: ToastDetails.error(),
+    );
+  }
+
+  // download wallpaper func
+
+  String fPath = '';
+  Future<String> downloadWallpaper() async {
+    FileDownloader.downloadFile(
+      url: widget.imgUrl,
+      onProgress: (fileName, progress) {
+        setState(
+          () {
+            _progress = progress;
+          },
+        );
+      },
+      onDownloadCompleted: (path) async {
+        print('path: $path');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('File Saved at: $path'),
+          ),
+        );
+
+        openFile(path);
+        fPath = path;
+        setState(() {
+          _progress = null;
+        });
+      },
+    ).then((value) {
+      print('Download Successful');
+      count++;
+    });
+    return fPath;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      floatingActionButton: ElevatedButton(
-        onPressed: () {
-          FileDownloader.downloadFile(
-            url: widget.imgUrl,
-            onProgress: (fileName, progress) {
-              setState(
-                () {
-                  _progress = progress;
-                },
-              );
-            },
-            onDownloadCompleted: (path) async {
-              print('path: $path');
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('File Saved at: $path'),
-                ),
-              );
-
-              openFile(path);
-
-              setState(() {
-                _progress = null;
-              });
-            },
-          ).then((value) {
-            print('Download Successful');
-          });
-        },
-        child: Text('Download Wallpaper'),
-      ),
+      floatingActionButton: count == 0
+          ? ElevatedButton(
+              onPressed: () {
+                downloadWallpaper();
+              },
+              child: Text('Download Wallpaper'),
+            )
+          : ElevatedButton(
+              onPressed: () {
+                setWallpapers(fPath);
+              },
+              child: Text('Set Wallpaper')),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(widget.imgUrl), fit: BoxFit.cover),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(widget.imgUrl), fit: BoxFit.cover),
+            ),
+          ),
+        ],
       ),
     );
   }
