28fc39e4f219f92bb99980817911429efbcaeeab
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-07-03 03:37:47 +0530

    Minor Changes 3

--- lib/controllers/api.dart
+++ lib/controllers/api.dart
@@ -12,10 +12,7 @@ class APIs {
     print('value obtained');
     await http.get(
       Uri.parse('https://api.pexels.com/v1/curated'),
-      headers: {
-        'Authorization':
-            '[YOUR API KEY]'
-      },
+      headers: {'Authorization': '[YOUR API KEY]'},
     ).then((value) {
       Map<String, dynamic> jsonData = jsonDecode(value.body);
       List photos = jsonData['photos'];
@@ -33,10 +30,7 @@ class APIs {
     await http.get(
       Uri.parse(
           'https://api.pexels.com/v1/search?query=$query&per_page=98&page=1'),
-      headers: {
-        'Authorization':
-            'HhkLKrmFtvFbWun0fkRROQoAgJDgnLDuGvDqGccF1YusqBad2pziI0o4'
-      },
+      headers: {'Authorization': '[YOUR API KEY]'},
     ).then((value) {
       Map<String, dynamic> jsonData = jsonDecode(value.body);
       List photos = jsonData['photos'];
